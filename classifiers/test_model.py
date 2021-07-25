2a92c2bb54e93db39cb1799d4aa39e5b0516ee43
Matthewchen37 <matt.chen42601@gmail.com>
2021-07-25 22:35:34 +0000

    Clean up scoring

--- classifiers/test_model.py
+++ classifiers/test_model.py
@@ -22,24 +22,20 @@ def score_model(model, X_test, Y_test):
     avg_prec_score_micro = precision_score(y_pred, Y_test, average='micro')
     avg_prec_score_macro = precision_score(y_pred, Y_test, average='macro')
     avg_prec_score_weighted = precision_score(y_pred, Y_test, average='weighted')
-    #avg_prec_score_samples = precision_score(y_pred, y_test, average='samples')
     avg_prec_score_overall = (avg_prec_score_micro + avg_prec_score_macro + avg_prec_score_weighted) / 3
 
     print(f"Model precision (micro): {avg_prec_score_micro}")
     print(f"Model precision (macro): {avg_prec_score_macro}")
     print(f"Model precision (weighted): {avg_prec_score_weighted}")
-    #print(f"Model precision (samples): {avg_prec_score_samples}")
 
     avg_recall_score_micro = recall_score(y_pred, Y_test, average='micro')
     avg_recall_score_macro = recall_score(y_pred, Y_test, average='macro')
     avg_recall_score_weighted = recall_score(y_pred, Y_test, average='weighted')
-    #avg_recall_score_samples = recall_score(y_pred, y_test, average='samples')
     avg_recall_score_overall = (avg_recall_score_micro + avg_recall_score_macro + avg_recall_score_weighted ) / 3
 
     print(f"Model recall (micro): {avg_recall_score_micro}")
     print(f"Model recall (macro): {avg_recall_score_macro}")
     print(f"Model recall (weighted): {avg_recall_score_weighted}")
-    #print(f"Model recall (samples): {avg_recall_score_samples}")
 
     accuracy = model.score(X_test, Y_test)
     print(f"Model accuracy: {accuracy}")
