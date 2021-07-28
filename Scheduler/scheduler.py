4210d82a802143d321c09f460a68f132d89e4520
Matthewchen37 <matt.chen42601@gmail.com>
2021-07-28 16:55:57 +0000

    Spaghetti that works?

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -9,6 +9,47 @@ import pickle as pkl
 import time
 import pandas as pd
 import multiprocessing as mp
+from multiprocessing.managers import BaseManager, MakeProxyType, public_methods
+from queue import PriorityQueue, Empty
+
+class ProxyPriorityQueue(PriorityQueue):
+	def get_attribute(self, name):
+		return getattr(self, name)
+
+
+class CustomManager(BaseManager):
+	pass
+
+class file_estimated_cost:
+	def __init__(self, file_name, probabilities, sizes, times):
+		self.file_name = file_name
+		self.probabilities = probabilities
+		self.sizes = sizes
+		self.times = times
+		self.costs = -1 * np.log(np.multiply(self.sizes, 
+							np.multiply(self.probabilities, self.times)) 
+							+ np.finfo(float).eps)
+	def best_extractor(self):
+		return np.amax(-1 * self.costs)
+	def best_extractor_index(self):
+		return np.argmax(-1 * self.costs)
+	def get_probabilities(self):
+		return self.probabilities
+	def get_sizes(self):
+		return self.sizes
+	def get_times(self):
+		return self.times
+	def get_filename(self):
+		return self.file_name
+	def __repr__(self):
+		return "File path: " + self.file_name + " Cost: " + str(self.best_extractor())
+	def __lt__(self, other):
+		return self.best_extractor() > other.best_extractor()
+
+class ProxyFileEstimatedCost(file_estimated_cost):
+	def get_attribute(self, name):
+		return getattr(self, name)
+
 
 class Scheduler:
 	def __init__(self, class_table_path, sampler_model_file_path, time_model_directory, size_model_directory, file_crawl_map_path, actual_extraction_times, test=False):
@@ -26,18 +67,30 @@ class Scheduler:
 		self.test = test
 		self.file_crawl_map = pd.read_csv(file_crawl_map_path)
 		
-		self.crawl_queue = []
-		self.xtract_queue = []
-
+		self.manager = self.get_manager()
+		self.crawl_queue = self.manager.PriorityQueue()
+		self.xtract_queue = self.manager.PriorityQueue()
+		self.file_index = mp.Value('i', 0)
 
 	def simulate_crawl(self):
 		for i in range(len(self.file_crawl_map.index)):
-			heapq.heappush(self.crawl_queue, (self.file_crawl_map["crawl_timestamp"][i], self.file_crawl_map["petrel_path"][i]))
+			self.crawl_queue.put((self.file_crawl_map["crawl_timestamp"][i], self.file_crawl_map["petrel_path"][i]))
 			if i == 0:
 				elapsed_time = self.file_crawl_map["crawl_timestamp"][i]
 			else:
 				elapsed_time = self.file_crawl_map["crawl_timestamp"][i] - self.file_crawl_map["crawl_timestamp"][i - 1]
 			time.sleep(elapsed_time)
+
+
+	def get_manager(self):
+		PriorityQueueProxy = MakeProxyType("PriorityQueue", public_methods(PriorityQueue))
+		FileEstimatedCostProxy = MakeProxyType("file_estimated_cost", public_methods(file_estimated_cost))
+
+		CustomManager.register("PriorityQueue", PriorityQueue, PriorityQueueProxy)
+		#CustomManager.register("file_estimated_cost", file_estimated_cost, FileEstimatedCostProxy)
+		m = CustomManager()
+		m.start()
+		return m
 
 	def run(self, directory_path):
 		start_time = time.time()
@@ -46,9 +99,10 @@ class Scheduler:
 		if mp.cpu_count() % 2 != 0:
 			print("This program only works on even-cored processors")
 			exit()
+
 		
 		enqueue_processes=[mp.Process(target=self.enqueue, args=(lock,)) for x in range(0, int(mp.cpu_count() / 2))]
-		dequeue_processes=[mp.Process(target=self.dequeue, args=(lock,)) for x in range(0, int(mp.cpu_count() / 2))]
+		dequeue_processes=[mp.Process(target=self.dequeue, args=(lock, self.file_index)) for x in range(0, int(mp.cpu_count() / 2))]
 
 		self.simulate_crawl() 
 
@@ -58,43 +112,58 @@ class Scheduler:
 		for p in dequeue_processes:
 			p.start()
 		
+		for p in dequeue_processes:
+			p.join()
+
 		for p in enqueue_processes:
 			p.join()
 
-		for p in dequeue_processes:
-			p.join()
 	
 		print("--- %s seconds ---" % (time.time() - start_time))
 
 	def enqueue(self, lock):
-		while len(self.crawl_queue) != 0:
-			lock.acquire()
-			file_tuple = heapq.heappop(self.crawl_queue)
-			lock.release()
+		while not self.crawl_queue.empty():
+			#print("Enqueue file tuple lock")
+			#lock.acquire()
+			#try:
+			file_tuple = self.crawl_queue.get()
+			#finally:
+			#	print("Enqueue file tuple lock released")
+			#	lock.release()
 			_, file_path = file_tuple
 			file_cost = self.calculate_costs(file_path)
-			lock.acquire()
-			heapq.heappush(self.xtract_queue, file_cost)
-			lock.release()
+			
+			#print("Enqueue push onto the heap lock")
+			#lock.acquire()
+			#try:
+			self.xtract_queue.put(file_cost)
+			#finally:
+			#	print("Enqueue push onto the heap lock released")
+			#	lock.release()
 
 
-	def dequeue(self, lock):
-		while len(self.xtract_queue) != 0 or len(self.crawl_queue) != 0:
-			lock.acquire()
+	def dequeue(self, lock, file_index):
+		while not self.xtract_queue.empty() or not self.crawl_queue.empty():
+			#lock.acquire()
 			file_cost = None
 			try:
-				file_cost = heapq.heappop(self.xtract_queue)
+				file_cost = self.xtract_queue.get(timeout=120)
 			except IndexError:
+				print("Nothing in xtract queue")
 				pass	
-			finally:
-				lock.release()
+			except Empty:
+				print("Took too long to get from queue")
+				pass
+			#finally:
+				#lock.release()
 			if file_cost != None:
 				best_index = file_cost.best_extractor_index()
 				if best_index == 2: #skip unknowns
 					continue
 				extraction_time = self.extraction_times.loc[file_cost.get_filename()][best_index]
 				time.sleep(extraction_time)
-				print("Dequeue")
+				file_index.value += 1
+				print("Dequeue:", file_index.value, "Extraction time:", extraction_time)
 
 
 	def calculate_costs(self, filename):
@@ -189,31 +258,6 @@ class Scheduler:
 	def get_crawl_queue(self):
 		return self.crawl_queue
 
-class file_estimated_cost:
-	def __init__(self, file_name, probabilities, sizes, times):
-		self.file_name = file_name
-		self.probabilities = probabilities
-		self.sizes = sizes
-		self.times = times
-		self.costs = -1 * np.log(np.multiply(self.sizes, 
-							np.multiply(self.probabilities, self.times)) 
-							+ np.finfo(float).eps)
-	def best_extractor(self):
-		return np.amax(-1 * self.costs)
-	def best_extractor_index(self):
-		return np.argmax(-1 * self.costs)
-	def get_probabilities(self):
-		return self.probabilities
-	def get_sizes(self):
-		return self.sizes
-	def get_times(self):
-		return self.times
-	def get_filename(self):
-		return self.file_name
-	def __repr__(self):
-		return "File path: " + self.file_name + " Cost: " + str(self.best_extractor())
-	def __lt__(self, other):
-		return self.best_extractor() > other.best_extractor()
 
 if __name__ == "__main__":
 	scheduler = Scheduler(
