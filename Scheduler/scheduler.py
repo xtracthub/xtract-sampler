abdc2b3f220681c607d35367c0eb5990e97471e3
matthewchen37 <matt.chen42601@gmail.com>
2022-02-14 21:00:09 -0500

    Start Analysis

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -3,6 +3,7 @@ import pickle
 from math import log
 import numpy as np
 import json
+import csv
 
 class Scheduler:
 
@@ -18,31 +19,52 @@ class Scheduler:
 
 	def run(self, prob_vectors, file_sizes):
 		priority_list = []
+		error_files = set()
 
+		file_sizes_dict = self.parse_file_sizes(file_sizes)
 
 		with open(prob_vectors, "r") as f:
 			data = json.load(f)
+			count = 0
+			pairs_processed = 0
 			for name, vector in data.items():
-				for extractor, probability in vector.items():
+				if count % 8000 == 0:
+					print("Curr count:", count)
 
-					time_model = self.model_piles[extractor]['extraction_time']
-					if isinstance(time_model, float):
-						expected_time = time_model
-					else:
-						expected_time = time_model.predict(file_sizes[name])
-					
-					size_model = self.model_piles[extractor]['extraction_size']
+				extrac_count = 0
+				for extractor, probability in vector['probabilities'].items():
+					if extractor in self.model_piles:
+						expected_size = None
+						expected_time = None
 
-					if isinstance(size_model, float):
-						expected_size = size_model
-					else:
-						expected_size = size_model.predict(file_sizes[name])
-			
-					priority_value = self.calculate_benefit(self, probability, expected_time, expected_size)
+						time_model = self.model_piles[extractor]['extraction_time']
+						if isinstance(time_model, float):
+							expected_time = time_model
+						else:
+							try:
+								expected_time = time_model.predict(file_sizes_dict[name])
+							except KeyError:
+								error_files.add((name, count))
+						size_model = self.model_piles[extractor]['extraction_size']
 
-					priority_list.append((name, extractor, priority_value))
+						if isinstance(size_model, float):
+							expected_size = size_model
+						else:
+							expected_size = size_model.predict(file_sizes_dict[name])
 
-		return priority_list
+						if expected_time != None and expected_size != None:
+							priority_value = self.calculate_benefit(probability, expected_time, expected_size)
+							priority_list.append((name, extractor, priority_value))
+							pairs_processed += 1
+						extrac_count += 1
+
+				count += 1
+		print("Files processed: ", count)
+		print("Pairs Processed:", pairs_processed)
+
+
+
+		return priority_list, list(error_files)
 	
 	# Was called calculating the cost but now we want to frame it as calculating the benefit
 	# our objective function
@@ -51,18 +73,27 @@ class Scheduler:
 		benefit_raw = sizes_probability / (expected_time + np.finfo(float).eps + 1) # so we don't divide by zero
 		return -1 * log(benefit_raw) # priority sorts in increasing order so we flip it around 
 
+	def parse_file_sizes(self, file_sizes):
+		file_sizes_dict = dict()
+
+		with open(file_sizes, "r") as f:
+			csv_reader = csv.DictReader(f)
+			for row in csv_reader:
+				file_sizes_dict[row["path"]] = np.array([row["size"]]).reshape(1, -1)
+
+		return file_sizes_dict
+		
+
+
 
 if __name__ == "__main__":
-
-
 	scheduler = Scheduler(os.path.abspath("cdiac_model_piles/"))
+	output, error_files = scheduler.run("cdiac_probability_predictions.json", "csv-try-2.csv")
 
+	print("Error files: ", len(error_files))
 
+	with open('cdiac_priority_list_2', 'wb+') as out:
+		pickle.dump(output, out)
 
-	with open("cdiac_probability_predictions.json") as f:
-		data = json.load(f)
-		for name, value in data.items():
-			print(name)
-			for key, value in value['probabilities'].items():
-				print(key, value)
-			break
+	with open('cdiac_error_files_2', 'wb+') as out:
+		pickle.dump(error_files, out)
